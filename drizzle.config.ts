408d120372ba1d5a2f86b310fb4c56f218bd50b8
Aydin Terzi <aydinterzi7@gmail.com>
2024-05-12 13:09:53 +0300

    bug solved

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   dbCredentials: {
-    url: process.env.DRIZZLE_DATABASE_URL,
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL!);
 const db = drizzle(sql);
 
 export default db;
